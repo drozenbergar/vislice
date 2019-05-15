752dd90e43f6651a9131446c1ccaf3f3c2f70b79
Dalia Roženbergar <dalia.rozenbergar@student.fmf.uni-lj.si>
2019-05-15 15:28:14 +0200

    preimenovanje besed in dodatek k modelu

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
 # Vislice
 Repozitorij za skupni porjekt pri **UVP**
 
-## Matematiki
-
-- Ivan Vidav
-
-- Matija Pretnar
-
-- Anja Petkovič
-
--- model.py
+++ model.py
@@ -45,11 +45,28 @@ class Igra:
         return delni
     
     def nepravilni_ugibi(self):
-        
+        return ' '.join(self.napacne_crke())
+
+    def ugibaj(self, crka):
+        crka = crka.upper()
+        if crka in self.crke:
+            return PONOVLJENA_CRKA
+        else:
+            self.crke.append(crka)
+        if crka in self.geslo:
+            if self.zmaga():
+                return ZMAGA
+            else:
+                return PRAVILNA_CRKA
+        else:
+            if self.poraz():
+                return PORAZ
+            else:
+                return NAPACNA_CRKA
 
 
-testno_geslo = "požrtvovalnost"
-testne_crke = ["a", "e", "o", "p"]
+testno_geslo = "požrtvovalnost".upper()
+testne_crke = ["A", "E", "O", "P"]
 zmagovalne_crke = [x for x in testno_geslo]
 igra = Igra(testno_geslo, testne_crke)
 print(igra.napacne_crke())
@@ -60,3 +77,13 @@ zmagana_igra = Igra(testno_geslo, zmagovalne_crke)
 print(zmagana_igra.zmaga())
 print(igra.poraz())
 print(igra.pravilni_del_gesla())
+print(igra.nepravilni_ugibi())
+poskus = igra.ugibaj("r")
+print(poskus)
+print(igra.pravilni_del_gesla())
+poskus = igra.ugibaj("e")
+print(poskus)
+print(igra.pravilni_del_gesla())
+poskus = igra.ugibaj("X")
+print(poskus)
+print(igra.napacne_crke())
